aadd391a5b66af808a95302c3667f55edb8453c1
Shaik Kashif <kashifcodes10@gmail.com>
2026-06-09 01:43:10 +0530

    Making  a GTA VI Promotional Website

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const App = () => {
+  let [showContent, setShowContent] = useState(false);
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "Power4.easeInOut",
+      transformOrigin: "50% 50%",
+    }).to(".vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      ease: "Expo.easeInOut",
+      opacity: 0,
+      delay: -1.8,
+      transformOrigin: "50% 50%",
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
+          setShowContent(true);
+          this.kill();
+        }
+      },
+    });
+  });
+  return (
+    <>
+      <div>
+        <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+          <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+            <defs>
+              <mask id="viMask">
+                <rect width="100%" height="100%" fill="black" />
+                <g className="vi-mask-group">
+                  <text
+                    x="50%"
+                    y="50%"
+                    fontSize="250"
+                    textAnchor="middle"
+                    fill="white"
+                    dominantBaseline="middle"
+                    fontFamily="Arial Black"
+                  >
+                    VI
+                  </text>
+                </g>
+              </mask>
+            </defs>
+            <image
+              href="./bg.png"
+              width="100%"
+              height="100%"
+              preserveAspectRatio="xMidYMid slice"
+              mask="url(#viMask)"
+            />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
